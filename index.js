5b437d3c751e94117f3a4937e063c6bdffa71481
Cristina Murillo <murillocam96@gmail.com>
2018-11-04 19:36:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
@@ -20,4 +18,24 @@
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
-// }
+}
+
+BoardMember.prototype.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+
+BoardMember.prototype.releasePressStatement = () => {
+    return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.veto = function (){
+    return 'No, I must disagree'
+}
+
+BoardMember.prototype.doCharity = function () {
+    return "I like to help people."
+}
+
+BoardMember.prototype.approve = () => {
+    return 'You can do that!'
+}
